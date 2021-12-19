dcd290377f3bd9921b5fcc9c793d7d2b9516c4ef
Елена Федянина <semenovaea20@yandex.ru>
2021-12-19 18:19:08 +0500

    Интерфейс сервиса управления сотрудника

---
+++ WebStore/Services/Interfaces/IEmployeeData.cs
@@ -0,0 +1,15 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IEmployeeData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? GetEmployee(int id);
+
+        int Add(Employee employee);
+        bool Edit(Employee employee);
+
+        bool Delete(int id);
+    }
+}
