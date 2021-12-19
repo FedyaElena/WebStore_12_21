15afef8fc98db594e9dfe87c92beda313cc59868
Елена Федянина <semenovaea20@yandex.ru>
2021-12-19 17:02:57 +0500

    Выселение сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,27 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using WebStore.Data;
 using WebStore.Models;
-
 
 namespace WebStore.Controllers
 {
-    [Route("empl/[action]/{Id?}")]
-    [Route("Staff/[action]/{Id?}")]
+    //[Route("empl/[action]/{Id?}")]
+    //[Route("Staff/[action]/{Id?}")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
+        private readonly ICollection<Employee> __Employees;
+        public EmployeesController()
         {
-            new Employee { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 23, EmploymentDate = new DateTime(2021, 12, 01) },
-            new Employee { Id = 2, LastName = "Petrov", FirstName = "Petr", Patronymic = "Petrovich", Age = 24, EmploymentDate = new DateTime(2019, 07, 09) },
-            new Employee { Id = 3, LastName = "Sidorov", FirstName = "Semen", Patronymic = "Semenovich", Age = 28, EmploymentDate = new DateTime(2020, 10, 11) },
-        };
-     
+            __Employees = TestData.Employees;
+        }
+
         public IActionResult Index()
         {
             var result = __Employees;
             return View(result);
         }
-        [Route("~/employees/info-{id}")]
+        //[Route("~/employees/info-{id}")]
         public IActionResult Details(int Id)
         {
             ViewData["TestValue"] = 123;            
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Models;
+namespace WebStore.Data;
+
+public static class TestData
+{
+   public static List<Employee> Employees { get; } = new()
+       {
+           new Employee { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 23, EmploymentDate = new DateTime(2021, 12, 01) },
+           new Employee { Id = 2, LastName = "Petrov", FirstName = "Petr", Patronymic = "Petrovich", Age = 24, EmploymentDate = new DateTime(2019, 07, 09) },
+           new Employee { Id = 3, LastName = "Sidorov", FirstName = "Semen", Patronymic = "Semenovich", Age = 28, EmploymentDate = new DateTime(2020, 10, 11) },
+       };
+
+}
