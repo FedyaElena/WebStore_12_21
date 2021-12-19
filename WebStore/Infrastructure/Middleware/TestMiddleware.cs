374bcb8a67a8ebd4ed185363165576e3ee0b924c
Елена Федянина <semenovaea20@yandex.ru>
2021-12-19 15:53:43 +0500

    Пример промежуточного ПО

---
+++ WebStore/Infrastructure/Middleware/TestMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace WebStore.Infrastructure.Middleware;
+
+public class TestMiddleware
+{
+    private readonly RequestDelegate _Next;
+    public TestMiddleware(RequestDelegate Next)
+    {
+        _Next = Next;
+    }
+
+    public async Task Invoke(HttpContext Context)
+    {
+        var processning_task = _Next(Context);
+
+        await processning_task;
+        //await _Next(Context); 
+    }
+}
