1be1f8755460a356644589a1f70993da8679c512
Елена Федянина <semenovaea20@yandex.ru>
2021-12-23 09:29:34 +0500

    Валидация

--- WebStore/ViewModels/EmployeeEditViewModel.cs
+++ WebStore/ViewModels/EmployeeEditViewModel.cs
@@ -1,11 +1,34 @@
-﻿namespace WebStore.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace WebStore.ViewModels;
+
 
 public class EmployeeEditViewModel
 {
+    [HiddenInput(DisplayValue = false)]
     public int Id { get; set; }
+    
+    [Display(Name = "Фамилия")]
+    [Required(ErrorMessage = "Фамилия обязательна")]
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 255 символов")]
+    [RegularExpression(pattern: @"(([А-ЯЁ][а-яё]+)|([A-Z][a-z]+))?", ErrorMessage = "Ошибка формата")]
     public string LastName { get; set; }
+    
+    [Display(Name = "Имя")]
+    [Required(ErrorMessage = "Имя обязательно")]
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 255 символов")]
+    [RegularExpression(pattern: @"(([А-ЯЁ][а-яё]+)|([A-Z][a-z]+))?", ErrorMessage = "Ошибка формата")]
     public string FirstName { get; set; }
+
+    [Display(Name = "Отчество")]
+    [StringLength(255, ErrorMessage = "Длина отчества должна быть от 2 до 255 символов")]
+    [RegularExpression(pattern: @"(([А-ЯЁ][а-яё]+)|([A-Z][a-z]+))?", ErrorMessage = "Ошибка формата")]
     public string Patronymic { get; set; }
+
+    [Display(Name = "Возраст")]
+    [Range(18, 80, ErrorMessage = "Возраст должен быть в пределах от 18 до 80 лет")]
     public int Age { get; set; }
+
+    [Display(Name = "Дата приема на работу")]
     public DateTime EmploymentDate { get; set; }
 }
