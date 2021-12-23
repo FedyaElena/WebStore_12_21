6b97ba8d14bae6b8112a13c7669e41eb2687c872
Елена Федянина <semenovaea20@yandex.ru>
2021-12-23 11:29:56 +0500

    реализация сервиса каталога товаров + регистрация его в контейнере

---
+++ WebStore/Services/InMemoryProductData.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Models;
+using WebStore.Services.Interfaces;
+using WebStore.Domain.Entities;
+using WebStore.Data;
+
+namespace WebStore.Services;
+public class InMemoryProductData : IProductData
+{
+    public IEnumerable<Section> GetSections() => TestData.Sections;
+
+    public IEnumerable<Brand> GetBrands() => TestData.Brands;
+}
+
