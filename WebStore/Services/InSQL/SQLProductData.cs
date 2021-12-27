5f308d8ff3e0ce3097730ccd2f8433dab12f006d
Елена Федянина <semenovaea20@yandex.ru>
2021-12-27 20:00:12 +0500

    Реализация сервиса каталога товаров в БД

---
+++ WebStore/Services/InSQL/SQLProductData.cs
@@ -0,0 +1,33 @@
+﻿using WebStore.Models;
+using WebStore.Services.Interfaces;
+using WebStore.Domain.Entities;
+using WebStore.Data;
+using WebStore.Domain;
+using WebStore.DAL.Context;
+
+namespace WebStore.Services.InSQL;
+
+public class SQLProductData : IProductData
+ {
+    private readonly WebStoreDB _db;
+
+    public SQLProductData(WebStoreDB db) => _db = db;
+
+    public IEnumerable<Section> GetSections() => _db.Sections;
+
+    public IEnumerable<Brand> GetBrands() => _db.Brands;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+       IQueryable<Product> query = _db.Products;
+
+        if (Filter?.SectionId is { } section_id)
+            query = query.Where(p => p.SectionId == section_id);
+
+        if (Filter?.BrandId is { } brand_id)
+            query = query.Where(p => p.BrandId == brand_id);
+
+        return query;
+    }
+}
+
